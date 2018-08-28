efb8c905ecc7bf44b2ad9c51f5b2d4ee71d20031
deekshithas <sdeekshitha258@gmail.com>
2018-08-28 14:27:21 +0530

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,28 @@
+
+#bitwise operation
+import cv2
+import numpy as np
+square=np.zeros((300,300),np.uint8)
+cv2.rectangle(square,(50,50),(250,250),255,-1)
+cv2.imshow("square",square)
+cv2.waitKey(0)
+#making as ellipse
+ellipse=np.zeros((300,300),np.uint8)
+cv2.ellipse(ellipse,(150,150),(150,150),30,0,180,255,-1)
+cv2.imshow("ellipse",ellipse)
+cv2.waitKey(0)
+And=cv2.bitwise_and(square,ellipse)
+cv2.imshow("and",And)
+cv2.waitKey(0)
+Or=cv2.bitwise_or(square,ellipse)
+cv2.imshow("or",Or)
+cv2.waitKey(0)
+Xor=cv2.bitwise_xor(square,ellipse)
+cv2.imshow("Xor",Xor)
+cv2.waitKey(0)
+Not=cv2.bitwise_not(square,ellipse)
+cv2.imshow("Not",Not)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
